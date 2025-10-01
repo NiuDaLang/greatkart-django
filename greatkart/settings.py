bda99f0c95543d5270687d507c601572867bcdfe
NiuDaLang <gogocfa@yahoo.co.jp>
2025-10-02 00:20:15 +1000

    add env and CNAME

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG", default=True, cast=bool) # True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["django-greatkart-env.eba-tmz2mdpy.us-west-2.elasticbeanstalk.com"]
 SECURE_CROSS_ORIGIN_OPENER_POLICY = "same-origin-allow-popups"
 
 
