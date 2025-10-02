4d8c7ff676f12762a8b7300a3732d854e0c50961
NiuDaLang <gogocfa@yahoo.co.jp>
2025-10-02 12:13:19 +1000

    restart env

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG", default=True, cast=bool) # True
 
-ALLOWED_HOSTS = ["django-greatkart-env.eba-tmz2mdpy.us-west-2.elasticbeanstalk.com"]
+ALLOWED_HOSTS = ["*"]
 SECURE_CROSS_ORIGIN_OPENER_POLICY = "same-origin-allow-popups"
 
 
